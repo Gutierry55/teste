a45801d3701a1b49c86353c940404604b66efec9
Gutierry55 <vitoogutierry1@gmail.com>
2025-05-14 20:10:42 -0300

    Primeiro commit

---
+++ CSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿int numero = 1; 
+string nome = "Arthur";
+char letra = 'A';
+decimal moeda = 3.9m;
+double numeroDecimal = 4.9;
+
+
+Console.WriteLine("O numero é: "+numero);
+Console.WriteLine("O numero decimal é: "+numeroDecimal);
+Console.WriteLine("A moeda é: "+moeda);
+Console.WriteLine("A letra é: "+letra);
+Console.WriteLine("O nome é: "+nome);
+
+if(numero == 1){
+    Console.WriteLine("O numero é 1");
+
+}
+
+else{
+    Console.WriteLine("O numero não é 1");
+}
+
+for(int i = 0; i < 10; i++)
+{ 
+ Console.WriteLine(i);
+}
+
+for(int i = 0; i < 10; i--)
+{ 
+ Console.WriteLine(i+ "...");
+ System.Threading.Thread
+}
